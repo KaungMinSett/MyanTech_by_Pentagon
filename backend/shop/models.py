36eafcb008d52edd834da176512c425a34b0bbca
Yoon Thiri Aung <yoonthiriaung04@gmail.com>
2025-02-09 22:45:56 +0630

    commit

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -3,3 +3,6 @@ from django.conf import settings
 
 class Customer(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    full_name= models.CharField(max_length=100)
+    phone =models.CharField(max_length=15)
+
