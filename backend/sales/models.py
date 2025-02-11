468142cb47685def766eb0aa894c9c7686bfa3ca
KaungMinSett <KaungMinSat@my.uopeople.edu>
2025-02-11 15:27:31 +0630

    local image storage

--- backend/sales/models.py
+++ backend/sales/models.py
@@ -11,6 +11,7 @@ class Price(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_published = models.BooleanField(default=False)
 
+
     def get_available_stock(self):
 
         product = self.product_id
@@ -40,6 +41,7 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id} by {self.customer.full_name} on {self.order_date}"
+    
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_items")
     product_id = models.ForeignKey(Price,on_delete=models.CASCADE)
