9aaedf9f8753b007e31849d3cc72b2cf1eda0c99
YatiZ <zuzu62113@gmail.com>
2025-02-13 16:09:02 +0630

    Update product list table

--- admin-dashboard/src/mocks/sales/products.js
+++ admin-dashboard/src/mocks/sales/products.js
@@ -7,7 +7,7 @@ export const products = [
       "description": "High-performance laptop for professionals.",
       "total_quantity": 150,
       "price": "$1200",
-      "image": "https://via.placeholder.com/150" 
+      "image": "https://images.unsplash.com/photo-1511385348-a52b4a160dc2?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwY29tcHV0ZXJ8ZW58MHx8MHx8fDA%3D" 
     },
     {
       "id": 2,
