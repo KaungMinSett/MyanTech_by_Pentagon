1081372de6b74b0a030e29ea29390352f71a69d0
Direwen <direwensik@gmail.com>
2025-02-13 20:26:53 +0630

    Created Auth for Ecommerce

---
+++ ecommerce/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <MainLayout>
+    <RouterView></RouterView>
+  </MainLayout>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+import MainLayout from './layouts/MainLayout.vue';
+import { useAuthStore } from './stores/authStore';
+import { onMounted } from 'vue';
+
+const authStore = useAuthStore()
+onMounted(async () => {
+  await authStore.getUser()
+})
+</script>
---
+++ ecommerce/src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+
+<div class="drawer">
+        <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col">
+            <!-- Navbar -->
+            <Navbar />
+            <!-- Page content here -->
+            <div class="mt-16">
+                <slot></slot>
+            </div>
+
+            <Footer />
+        </div>
+        <div class="drawer-side">
+            <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
+            <ul class="menu bg-base-200 min-h-full w-80 p-4">
+                <!-- Sidebar content here -->
+                <li><a>Sidebar Item 1</a></li>
+                <li><a>Sidebar Item 2</a></li>
+            </ul>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+import Navbar from "../components/Navbar.vue"
+import Footer from "../components/Footer.vue"
+</script>
